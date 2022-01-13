941c6d9edd3cf73a7d32777f692079e21874e19c
mejiabrayan <learnwithbrayan@gmail.com>
2022-01-12 16:20:04 -0800

    Adding react components

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: path.join(__dirname, '/bundle'),
+        filename: 'index_bundle.js'
+
+    },
+    devserver: {
+        inline: true,
+        port: 8001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                presets: ['es2015', 'react']
+
+                    
+            }
+        }]
+},
+plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html'
+    })
+]
+}
